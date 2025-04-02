01008e9c7f74acf17cd1265c0bb8962b940b0dbc
edwardwq301 <61405994+edwardwq301@users.noreply.github.com>
2025-04-02 13:41:02 +0800

    Update exc.sh

--- exc.sh
+++ exc.sh
@@ -3,7 +3,7 @@ set -e  # 遇到错误立即退出
 
 # # 进入脚本所在目录
 # cd "$(dirname "$0")"
-echo $0
+
 # 1. 创建虚拟环境（如果不存在）
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
@@ -20,9 +20,9 @@ pip install -r requirements.txt
 # 4. 运行测试并生成报告
 echo "Running tests..."
 pytest app.py \
-    --junitxml=test-result/test-results.xml \
     --html=test-result/report.html \
     --self-contained-html \
+# --junitxml=test-result/test-results.xml \
     -v
 
 # 5. 返回测试结果（pytest会自动返回0/1）
